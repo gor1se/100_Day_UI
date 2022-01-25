290b3005e754f6a6585fcc18fb7e4ddeb5493f34
gor1se <andreas.schmidt.lth@t-online.de>
2022-01-25 14:15:42 +0100

    Challenge 9: Musik-Player eines Smartphones

---
+++ Challenge_009/app.js
@@ -0,0 +1,27 @@
+"use strict";
+
+//Image
+
+const img = document.querySelector(".cover-img");
+
+img.addEventListener("mouseover", () => {
+    img.classList.remove("img-smaller");
+    img.classList.add("img-bigger");
+});
+
+img.addEventListener("mouseleave", () => {
+    img.classList.remove("img-bigger");
+    img.classList.add("img-smaller");
+});
+
+// Play Button
+const playButton = document.querySelector(".btn-play-top");
+const playButtonItem = document.querySelector(".btn-play");
+
+playButton.addEventListener("click", () => {
+    if (playButtonItem.getAttribute("src") === "./img/play.png") {
+        playButtonItem.setAttribute("src", "./img/pause.png");
+    } else {
+        playButtonItem.setAttribute("src", "./img/play.png");
+    }
+});
